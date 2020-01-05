facc659087ffcebdcd41604dfe77647e13359df0
otigas <otigas@hotmail.com>
2020-01-05 17:11:22 +0200

    Assignment 1 - Practicing Components

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+// import component decorator from lib
+import { Component } from '@angular/core' ;
+
+// declare component decorator
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+// the class of the component
+
+export class ServerComponent {
+
+}
